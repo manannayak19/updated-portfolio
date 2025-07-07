dfb8653e86ae19e56bf90ae317dd1ffb713d43a4
Manan Nayak <nayakmanan1911@gmail.com>
2025-07-07 14:41:53 -0400

    font update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,7 @@ import "./about.css";
 import portfolio from "../assets/Group1.svg";
 import resume from "../assets/resume.pdf"
 
+
 const About = () => {
   return (
     <div className="w-full overflow-x-hidden">
@@ -12,8 +13,8 @@ const About = () => {
         </div>
 
         {/* left column */}
-        <div className="flex flex-col justify-center items-center w-full md:w-auto mb-4 md:mb-0">
-          <div className="flex flex-col md:flex md:flex-row">
+        <div className="flex flex-col justify-center items-center w-full md:w-auto mb-4 md:mb-0 ">
+          <div className="flex flex-col md:flex md:flex-row font-[m6x11]">
             {/* lnkedn */}
             <a href="https://www.linkedin.com/in/manan-nayak/" target="_blank">
               <button style={{ "background-color": "#0093ff" }}>
@@ -35,7 +36,7 @@ const About = () => {
         </div>
 
         {/* right column */}
-        <div className=" p-3 w-auto right-card md:w-3xl">
+        <div className=" p-3 w-auto right-card md:w-3xl font-[m6x11]">
           <div>
             Hello! I’m Manan, recent grad, I work across MERN stack. Always building and learning new stuff, Curently getting into ML and Web3. Contact me <a style={{"color":"#ff4c40"}} href="mailto:themanannayak@gmail.com">themanannayak@gmail.com</a>
           </div>
